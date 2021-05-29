049ae6c1db0629e3c794b30e6adfe1f1b274493f
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2021-05-29 08:54:28 +0700

    deployed machine learning

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const tf = require('@tensorflow/tfjs-node');
+const path = require('path');
+const vocab = require('../vocab/vobab.json');
+let model = null;
 
-router.get('/', function (req, res, next) {
-    res.send('ok');
+function paddingArray(sequence, padding) {
+    const insertPadding = 71 - padding.length;
+    for (let i = 0; i < insertPadding; i++) {
+        sequence.push(0);
+    }
+    return sequence;
+}
+
+function textToSequence(rawInput) {
+    const input = Array.isArray(rawInput) ? rawInput : [rawInput];
+    return input.reduce(function (finalResult, currentInput) {
+        const words = currentInput.split(" ");
+        const sequence = words.reduce(function (result, current) {
+            if (vocab[current]) result.push(vocab[current]);
+            return result;
+        }, []);
+        finalResult.push(paddingArray(sequence, 71));
+        return finalResult;
+    }, []);
+}
+
+function processOutput(output) {
+    return output.reduce(function (result, current) {
+        result.push(current[0]);
+        return result;
+    }, []);
+}
+
+router.get('/', async function (req, res) {
+    try {
+        if (!model) model = await tf.node.loadSavedModel(path.join(__dirname, '..', 'ml_models', 'spam_model'));
+        const input = textToSequence(req.query.input);
+        const result = model.predict(tf.tensor(input));
+        return res.json(processOutput(await result.array()));
+    } catch (e) {
+        console.log(e);
+        return res.send('error');
+    }
 });
 
 module.exports = router;
